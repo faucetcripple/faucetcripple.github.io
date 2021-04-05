1320d0edcde16082bf76416212b9e99a68fba61e
d74g0nlive <32942068+faucetcripple@users.noreply.github.com>
2021-04-05 02:03:07 -0400

    tweak gravity

--- use_doe.js
+++ use_doe.js
@@ -66,9 +66,9 @@ if (window.DeviceOrientationEvent) {
 function deviceOrientationListener(event) {
     document.getElementById('readoutA').textContent = Math.round(event.alpha);
     document.getElementById('readoutB').textContent = Math.round(event.beta);
-    global.scene.matter.world.engine.world.gravity.y = Math.round(event.beta) / 180;
+    global.scene.matter.world.engine.world.gravity.y = -(Math.round(event.beta) / 180)*2;
     document.getElementById('readoutG').textContent = Math.round(event.gamma);
-    global.scene.matter.world.engine.world.gravity.y = Math.round(event.gamma) / 90;
+    global.scene.matter.world.engine.world.gravity.y = -(Math.round(event.gamma) / 90)*2;
 }
     
     
