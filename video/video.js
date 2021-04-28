735275b9b435ad2e7a7114a3736cb6b173bea926
d74g0nlive <32942068+faucetcripple@users.noreply.github.com>
2021-04-28 01:21:50 -0400

    Works Half The Time.

--- video/video.js
+++ video/video.js
@@ -41,17 +41,8 @@ var loadingtext;
 
 
 function preload() {
-
-    //    loadingtext = this.add.text(100, 100, "Loading videos ...", {
-    //        font: "65px Arial",
-    //        fill: "#ff0044"
-    //    });
-
     //    this.load.video('space', '../common/video/love.mp4', 'loadeddata', true, true);
-    this.load.video('space', '../common/video/happyd.webm', 'loadeddata', true, true);
-
-
-
+    this.load.video('space', '../common/video/happyd.webm', 'loadeddata', true, false);
 }
 
 var video;
@@ -63,24 +54,37 @@ function create() {
 
     video.setVolume(0.5);
     video.setScale(0.3);
-
     video.setMute(false);
+    video.setLoop(false);
     video.play(true);
 
+    video.on('loop', function (video) {
+        console.log('finished');
+        //        video.stop(true);
+        window.location.href = '/SNAFU/index.html';
+
+
+    }, this);
+
+    video.on('complete', function (video) {
+        console.log('finished');
+        //        video.stop(true);
+        window.location.href = '/SNAFU/index.html';
+    }, this);
     cam = this.cameras.main;
 
-    //    this.tweens.add({
-    //        targets: cam,
-    //        zoom: {
-    //            from: 1,
-    //            to: 0.25
-    //        },
-    //        ease: 'Sine.easeOut', // 'Cubic', 'Elastic', 'Bounce', 'Back'
-    //        duration: 5000,
-    //        delay: 0,
-    //        repeat: -1, // -1: infinity
-    //        yoyo: true
-    //    });
+//    this.tweens.add({
+//        targets: cam,
+//        zoom: {
+//            from: 1,
+//            to: 0.25
+//        },
+//        ease: 'Sine.easeOut', // 'Cubic', 'Elastic', 'Bounce', 'Back'
+//        duration: 5000,
+//        delay: 0,
+//        repeat: -1, // -1: infinity
+//        yoyo: true
+//    });
 
     cam.setPostPipeline(EDGEBEND_HUE);
     cam.setPostPipeline(OLDMONITOR_FIXED);
